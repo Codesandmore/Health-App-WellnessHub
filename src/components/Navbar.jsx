1511967776defdb8d6c41b946a7384a24438af3f
CoderZ865 <118465159+CoderZ865@users.noreply.github.com>
2024-07-13 22:55:33 +0530

    updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,10 @@ import './Navbar.css'; // Import the CSS file
 const Navbar = () => (
   <nav className="navbar">
     <ul className="navbar-nav">
-      <li className="nav-item"><Link to="/form">Details</Link></li>
-      <li className="nav-item"><Link to="/bmi">BMI</Link></li>
+      <li className="nav-item"><Link to="/">Home</Link></li>
+      <li className="nav-item"><Link to="/bmi">Profile</Link></li>
       <li className="nav-item"><Link to="/leaderboard">Leaderboard</Link></li>
+      <li className="nav-item"><Link to="/">Logout</Link></li>
     </ul>
   </nav>
 );
